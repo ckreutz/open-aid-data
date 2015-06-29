69bbe85410078749cc16c6910e30ec75683a07b9
Christian Kreutz <ck@crisscrossed.de>
2015-06-29 12:27:45 +0200

    pep8

--- freeze.py
+++ freeze.py
@@ -2,9 +2,11 @@ from flask_frozen import Freezer
 from openaid import app
 
 freezer = Freezer(app)
+
+
 @freezer.register_generator
 def show_schwerpunkte():
-  yield '/sectors/all/'
+    yield '/sectors/all/'
 
 if __name__ == '__main__':
-  freezer.freeze()
+    freezer.freeze()
